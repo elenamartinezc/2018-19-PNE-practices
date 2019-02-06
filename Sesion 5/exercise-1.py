66fe7994ba010f01a30f2e879a4b55e7f714fb84
elenamc <emcarrasco4@gmail.com>
2019-02-06 17:12:38 +0100

    Testing

--- Sesion 5/exercise-1.py
+++ Sesion 5/exercise-1.py
@@ -1,4 +1,4 @@
-def count_a(seq):
+def counting(seq):
     """ Counting the number of As in the sequence """
 
     countera = 0
@@ -19,22 +19,5 @@ def count_a(seq):
     return instances
 
 dna = input("Please enter the sequence:")
-numberA = countera(dna)
-numberC = counterc(dna)
-numberG = counterg(dna)
-numberT = countert(dna)
-print ('The number of As is: {}'.format (numberA))
-print ('The number of Cs is: {}'.format (numberC))
-print ('The number of Gs is: {}'.format (numberG))
-print ('The number of Ts is: {}'.format (numberT))
+counting(dna)
 
-"""Calculate the total sequence length"""
-totalLength = len(dna)
-
-"""" Calculate the porcentage """
-if totalLength > 0:
-    perc = round (100.0 * numberA/totalLength , 1)
-else:
-    perc = 0
-
-print ("The total length is: {}". format (perc))

c5f117f31a92eec3d1670651047b97a427cf1ea1
elenamc <emcarrasco4@gmail.com>
2019-02-13 18:31:52 +0100

    practice 1

---
+++ Practice 2/serverP2.py
@@ -0,0 +1,45 @@
+import socket
+
+IP = "212.128.253.84"
+PORT = 8086
+MAX_OPEN_REQUESTS = 5
+connections = 0
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+# We reserve the port and the IP
+    s.bind((IP, PORT))
+
+# Maximum number of requests
+    s.listen(MAX_OPEN_REQUESTS)
+
+# We make an inifite loop
+    while True:
+        # We accept connections from outside
+        print("Waiting for connections at {}, {} ".format(IP, PORT))
+        (clientsocket, address) = s.accept()
+
+        # Counting the number of connections
+        connections += 1
+
+        # We print the number of connections
+        print("CONNECTION: {}. From the IP: {}".format(connections, address))
+
+        # We receive a message from the client
+        msg = clientsocket.recv(2048).decode("utf-8")
+        print("Message from client: {}".format(msg))
+
+        # We send the message back
+        message = "Hello from the teacher's server"
+        send_bytes = str.encode(message)
+        # We must write bytes, not a string
+        clientsocket.send(send_bytes)
+        clientsocket.close()
+
+except socket.error:
+    print("Problems using port {}. Do you have permission?".format(PORT))
+
+except KeyboardInterrupt:
+    print("Server stopped by the user")
+    serversocket.close()

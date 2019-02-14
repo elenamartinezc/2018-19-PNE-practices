61e928d35edf16f93cdff6a887ac1f6829e0c57e
elenamc <emcarrasco4@gmail.com>
2019-02-14 17:06:00 +0100

    practice 1

--- Practice 2/clientP2.py
+++ Practice 2/clientP2.py
@@ -1,7 +1,7 @@
 import socket
 from Seq import Seq
 
-IP = "212.128.253.84"
+IP = "212.128.253.114"
 PORT = 8086
 
 # We make an infinite loop
@@ -9,8 +9,8 @@ PORT = 8086
 while True:
     dna = input('Enter a sequence')
     seq = Seq(dna)
-    reverseSeq = str(seq.reverse())
-    complementSeq = str(seq.complement())
+    reverseSeq = seq.reverse()
+    complementSeq = seq.complement()
 
 # We create the socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,8 +19,10 @@ while True:
     s.connect((IP,PORT))
 
 # We send data
-    s.send(str.encode(reverseSeq))
-    s.send(str.encode(complementSeq))
+    seq1 = 'The complement seq is:' + complementSeq.strbase
+    seq2 = 'The reverse seq is:' + reverseSeq.strbase
+    s.send(str.encode(seq1))
+    s.send(str.encode(seq2))
 
 # We receive data
     msg = s.recv(2048).decode('UTF-8')
